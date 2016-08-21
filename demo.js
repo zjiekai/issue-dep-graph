0d098f36af98dfc014bee3d77124acd226357029
zjk <jiekai.zheng@transwarp.io>
2016-08-21 18:29:08 +0800

    add demo

--- demo.js
+++ demo.js
@@ -1,7 +1,47 @@
+function name2id(name) {
+  return data.issues[name].id;
+}
+
 function main() {
+  var taskData = [];
+  var linkData = [];
+  var idCounter = 0;
+
+  _.each(data.issues, function(issue, name) {
+    ++idCounter;
+    issue.id = idCounter;
+    var start_date = dateFormat(new Date(issue.created_at), 'dd-mm-yyyy');
+    taskData.push({
+      id: issue.id,
+      text: name,
+      start_date: start_date,
+      duration: 7
+    });
+  });
+
+  var linkIdCounter = 0;
+  _.each(data.graph, function(issues, name) {
+    try {
+      var id0 = name2id(name);
+      _.each(issues, function (name) {
+        var id1 = name2id(name);
+
+        ++linkIdCounter;
+        linkData.push({
+          id: linkIdCounter,
+          source: id1,
+          target: id0,
+          type: "0"
+        });
+      });
+    } catch(e) {
+      console.log(name);
+    }
+  });
+
   tasks = {
-    data:[],
-    links:[]
+    data: taskData,
+    links:linkData
   };
   gantt.init("gantt_here");
   gantt.parse(tasks);
